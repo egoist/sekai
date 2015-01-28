81e20215e916fad7d5d183f4344f12eda20b620c
0x142857 <0x142857@gmail.com>
2015-01-28 17:40:42 +0800

    add login in help menu

--- bin/insekai.js
+++ bin/insekai.js
@@ -5,6 +5,7 @@ require('colorful').toxic();
 if (!process.argv[2]) {
   console.log('sekai ~ ' + require('../package').version.green);
   console.log('Kono nice sekai in command line'.grey);
+  console.log('  $ '.cyan + 'sekai login');
   console.log('  $ '.cyan + 'sekai key YOUR_PRIVATE_KEY');
   console.log('  $ '.cyan + 'sekai say word');
   console.log('  $ '.cyan + 'sekai get public [page]');
